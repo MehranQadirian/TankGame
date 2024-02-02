6d31a2b22a4487f2576400dfce18c6ad5ff2a7c4
Mehran Qadirian <158446870+MehranQadirian@users.noreply.github.com>
2024-02-02 21:23:33 +0330

    Update ProgressBarColorModify.cs

--- ProgressBarColorModify.cs
+++ ProgressBarColorModify.cs
@@ -2,10 +2,17 @@
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 /*
- * This class can change the color of a ProgressBar to red, yellow and green
+ * This class can change the color of a ProgressBar to red, yellow and green.
+ *
  * If you want to use this class in your project, just use the SetStatus function
  * in front of a ProgressBar object that you have defined in your Windows Form project
  * and use the numbers 1, 2 and 3 in the parentheses.
+ * Example 1 : [ProgressBar_Name].SetStatus(1); => CHANGE COLOR PROGRESSBAR TO GREEN
+ * Example 2 : [ProgressBar_Name].SetStatus(2); => CHANGE COLOR PROGRESSBAR TO RED
+ * Example 3 : [ProgressBar_Name].SetStatus(3); => CHANGE COLOR PROGRESSBAR TO YELLOW
+ * 1 : GREEN
+ * 2 : RED
+ * 3 : YELLOW
  */
 namespace TankShooter
 {
